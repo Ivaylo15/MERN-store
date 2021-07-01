e3cbfcbfd195f118322b1ee45ed1d643065774f5
Ivaylo <ivaylopehlivanov@abv.bg>
2021-07-01 15:06:00 +0300

    trying to add tests with jest

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -1,3 +1,4 @@
+const statusCodes = require('../constants/status-codes');
 const Product = require('../models/Product');
 
 module.exports = {
@@ -8,7 +9,7 @@ module.exports = {
             const createdProduct = await Product.create({ title, category, size, color, price, image });
             res.send(createdProduct);
         } catch (e) {
-            res.status(500).json({ message: e.message });
+            res.status(statusCodes.InternalServerError).json({ message: e.message });
         }
     },
     editProduct: async (req, res, next) => {
@@ -18,7 +19,7 @@ module.exports = {
             const updatedProduct = await Product.updateOne({ _id: id }, { title, category, size, color, price, image });
             res.send(updatedProduct);
         } catch (e) {
-            res.status(500).json({ message: e.message });
+            res.status(statusCodes.InternalServerError).json({ message: e.message });
         }
     },
     deleteProduct: async (req, res, next) => {
@@ -28,7 +29,7 @@ module.exports = {
             const deletedProduct = await Product.deleteOne({ _id: id });
             res.send(deletedProduct);
         } catch (e) {
-            res.status(500).json({ message: e.message });
+            res.status(statusCodes.InternalServerError).json({ message: e.message });
         }
     },
     paginatedResults: async (req, res, next) => {
@@ -76,9 +77,9 @@ module.exports = {
             const rawResults = await Product.find(filterObject);
             results.results = await Product.find(filterObject).limit(limit).skip(startIndex).exec();
             results.pageCount = Math.ceil(rawResults.length / limit);
-            res.send(results)
+            res.status(statusCodes.OK).send(results)
         } catch (e) {
-            res.status(500).json({ message: e.message });
+            res.status(statusCodes.InternalServerError).json({ message: e.message });
         }
     },
     filterOptions: async (req, res, next) => {
@@ -107,7 +108,7 @@ module.exports = {
 
             res.send(filters);
         } catch (e) {
-            res.status(500).json({ message: e.message });
+            res.status(statusCodes.InternalServerError).json({ message: e.message });
 
         }
     }
