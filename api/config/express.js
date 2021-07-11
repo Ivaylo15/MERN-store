72b05d9392c835bd74e67930b5056e2b017c26c0
Ivaylo <ivaylopehlivanov@abv.bg>
2021-07-11 20:21:39 +0300

    Adding rete-limit and redis

--- api/config/express.js
+++ api/config/express.js
@@ -1,6 +1,13 @@
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const rateLimit = require("express-rate-limit");
+const constantValues = require('../constants/constantValues');
+
+const limiter = rateLimit({
+    windowMs: constantValues.RateLimitTimeFrame, // 15 minutes
+    max: constantValues.RateLimitRequests // limit each IP to 100 requests per windowMs
+  });
 
 module.exports = (app) => {
     app.use(cors({
@@ -15,4 +22,6 @@ module.exports = (app) => {
     app.use(bodyParser.json());
     
     app.use(cookieParser(process.env.COOKIE_SECRET));
+
+    app.use(limiter);
 };
--- api/controllers/product.js
+++ api/controllers/product.js
@@ -1,5 +1,9 @@
 const statusCodes = require('../constants/statusCodes');
 const Product = require('../models/Product');
+const Redis = require('redis');
+const constantValues = require('../constants/constantValues');
+
+const redisClient = Redis.createClient();
 
 module.exports = {
     addProduct: async (req, res, next) => {
@@ -76,10 +80,35 @@ module.exports = {
         }
 
         try {
+            // Can't figure out how to update redis after del/edit item yet
+            // redisClient.get('gallery', async (error, gallery) => {
+            //     if (error) console.log(error);
+            //     if (gallery !== null && gallery !== undefined && Object.keys(filterObject).length === 0 && Object.keys(sortObject).length === 0) {
+
+            //         const rawResults = await Product.find();
+            //         if (JSON.parse(gallery).results.length < limit && JSON.parse(gallery).results.length !== rawResults.length) {
+            //             results.results = await Product.find().limit(limit).skip(startIndex).exec();
+            //             results.pageCount = Math.ceil(rawResults.length / limit);
+            //             redisClient.setex('gallery', constantValues.RedisExpirationTime, JSON.stringify(results));
+            //             res.status(statusCodes.OK).send(results);
+            //             return;
+            //         }
+            //         return res.json(JSON.parse(gallery));
+            //     } else {
+            //         const rawResults = await Product.find(filterObject);
+            //         results.results = await Product.find(filterObject).sort(sortObject).limit(limit).skip(startIndex).exec();
+            //         results.pageCount = Math.ceil(rawResults.length / limit);
+            //         redisClient.setex('gallery', constantValues.RedisExpirationTime, JSON.stringify(results));
+            //         res.status(statusCodes.OK).send(results)
+            //     }
+            // })
+
             const rawResults = await Product.find(filterObject);
             results.results = await Product.find(filterObject).sort(sortObject).limit(limit).skip(startIndex).exec();
             results.pageCount = Math.ceil(rawResults.length / limit);
+            redisClient.setex('gallery', constantValues.RedisExpirationTime, JSON.stringify(results));
             res.status(statusCodes.OK).send(results)
+
         } catch (e) {
             res.status(statusCodes.InternalServerError).json({ message: e.message });
         }
@@ -118,8 +147,20 @@ module.exports = {
     singleProduct: async (req, res, next) => {
         const productId = req.params.id;
         try {
-            const result = await Product.findOne({ _id: productId });
-            res.status(statusCodes.OK).send(result);
+            const productData = await Product.findOne({ _id: productId });
+            redisClient.get(`product:${productId}`, async (error, product) => {
+                if (error) console.log(error);
+                if (product !== null) {
+                    if (JSON.stringify(productData) === product) {
+                        return res.json(JSON.parse(product));
+                    } else {
+                        redisClient.setex(key, constantValues.RedisExpirationTime, JSON.stringify(productData));
+                    }
+                } else {
+                    redisClient.setex(key, constantValues.RedisExpirationTime, JSON.stringify(productData));
+                }
+            })
+            res.status(statusCodes.OK).send(productData);
         } catch (e) {
             res.status(statusCodes.InternalServerError).json({ message: e.message });
         }
